b33e3d1a970087ccb6c2e09714321cc9475ae20e
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-07-15 13:56:48 -1000

    Update Hex dependencies

--- mix.exs
+++ mix.exs
@@ -47,9 +47,9 @@ defmodule GEPAFramework.MixProject do
   defp deps do
     DependencySources.deps(__DIR__) ++
       [
-        {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-        {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-        {:ex_doc, "~> 0.40.1", only: [:dev, :test], runtime: false}
+        {:credo, "~> 1.7.19", only: [:dev, :test], runtime: false},
+        {:dialyxir, "~> 1.4.7", only: [:dev, :test], runtime: false},
+        {:ex_doc, "~> 0.40.3", only: [:dev, :test], runtime: false}
       ]
   end
 
